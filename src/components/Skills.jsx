864ca7e03720e18b8e612b67058764e91f403fa9
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-09-17 14:36:58 +0200

    maj

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -93,17 +93,39 @@ const Skills = () => {
                 <Collapse
                   title={"Javascript"}
                   content={
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    "Je possède une bonne maîtrise de JavaScript, l'un des langages de programmation les plus puissants et polyvalents du web. Mon niveau de maîtrise s'étend au dela des bases pour le coté front-end, ce qui me permet de créer des applications web complexes et interactives. Je suis capable de manipuler le Document Object Model (DOM) avec JavaScript pour créer des pages web dynamiques. Cela inclut l'ajout, la suppression et la modification d'éléments HTML en temps réel. Je sais gérer les événements en JavaScript, que ce soit pour la gestion de clics, de survols de souris ou d'autres interactions, je peux créer des interfaces utilisateur réactives.  J'ai une bonne compréhension de la programmation orientée objet (POO) ce qui me permet de structurer mon code de manière propre et efficace. J'utilise des classes, des objets et l'héritage pour mettre en place des solutions efficaces dans le code. Je sais utiliser des opérations asynchrones en JavaScript en utilisant des promesses (Promises) et des fonctions asynchrones (async/await). Cela me permet de traiter des opérations telles que les appels API.  Je suis familier avec l'écosystème JavaScript, y compris les gestionnaires de paquets tels que npm et les outils de construction comme Webpack ou bien les frameworks front-end comme React. Je peux configurer des projets, gérer les dépendances , créer des projets from scratch. "
                   }
                 />
                 <Collapse
                   title={"Html"}
-                  content={"Urna neque viverra justo nec ultrices..."}
+                  content={
+                    "Je possède une solide maitrise en HTML , le langage fondamental pour la création de sites web. Je peux concevoir des mises en page complexes et attrayantes, en veillant à ce qu'elles soient compatibles avec différents navigateurs. De plus je maitrise la syntaxe Jsx pour react ce qui me rend polyvalent dans la construction d'un site simple en html ou bien à l'aide d'un frameworks comme react."
+                  }
                 />
-                <Collapse title={"Css"} content={"je test"} />
-                <Collapse title={"Sass"} content={"je test"} />
-                <Collapse title={"React"} content={"je test"} />
-                <Collapse title={"Responsive Desing"} content={"je test"} />
+                <Collapse
+                  title={"Css"}
+                  content={
+                    "Je possède une solide maîtrise en CSS, le second langage fondamental du web. Mon expertise en CSS me permet de créer des mises en pages solides et attrayante au visuel. Je mâitrise Flex et Grid , le positionnement , toutes les propriétés de bases , je sais utiliser SASS afin de mieux organiser le code , je sais intégrer une maquette de manière fidèle et optimisée."
+                  }
+                />
+                <Collapse
+                  title={"Sass"}
+                  content={
+                    "Je sais utiliser Sass pour simplifier et améliorer la gestion du style CSS. Cette technologie me permet de maintenir un code CSS propre et modulaire tout en aillant accès à des fonctionnalités telles que les variables."
+                  }
+                />
+                <Collapse
+                  title={"React"}
+                  content={
+                    "J'ai une bonne maîtrise de  React, je sais créer des interfaces utilisateur interactives. Je peux développer des composants réutilisables et structurer des applications web réactives avec plus de facilité, offrant ainsi une expérience utilisateur fluide. De plus ce portfolio est conçu avec React ce qui m'a permis d'améliorer mes compétences dans ce frameworks.Je sais gérer l'état local des composants avec les states et les props , je sais propager des données entre composants enfants et parents. Je sais utiliser React Routeur afin de gérer la navigation , je sais définir des routes , j'utilise des paramètres d'URL ou des liens. Je sais utiliser des bibliotheque comme Axios pour les appels API. Je sais intégrer des bibliotheques externes comme les packages NPM dans mon application React de manière efficace. Je sais déployer une application React , notament sur GitHub Pages. J'ai une bonne compréhension des hooks comme useState , useEffect ou useContext afin de gérer l'état et les effets dans les composants."
+                  }
+                />
+                <Collapse
+                  title={"Responsive Desing"}
+                  content={
+                    " Je sais concevoir un site qui s'adapte aux formats d'écrans, ce qui signifie que mes sites web sont conçus pour offrir une expérience utilisateur optimale sur divers appareils (desktop , tablet , mobile). Je maîtrise les media query afin d'adapter le site aux formats que je souhaites."
+                  }
+                />
               </div>
             </div>
             <div className="techno-back">
