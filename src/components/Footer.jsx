a41a31f03bad32f265f75ed609351c15e4f30050
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-10-18 09:43:25 +0200

    maj

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,6 @@ const Footer = () => {
         </div>
         <div className="me">
           <div className="name">
-            {" "}
             <span className="l">L</span>
             <span className="a">A</span>
           </div>
