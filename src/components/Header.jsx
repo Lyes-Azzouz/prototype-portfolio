a9f45b2fe959b8afcba555617654ac52e359e94c
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-10-26 14:05:01 +0200

    maj optimisation code responsive (répétition)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,40 +58,30 @@ const Header = () => {
               <span id="st-r">r</span>
             </div>
           </div>
-          <div className="about-me" onClick={toggleTexte}>
+
+          {texteVisible && (
+            <p>
+              Je suis Lyes, un développeur front-end junior. J'ai suivi une
+              formation chez Openclassroom, ce qui m'a permis de maîtriser le
+              développement web. J'ai acquis des compétences avancées grâce à
+              des projets concrets et travaille également sur le développement
+              back-end (Express.js, MongoDB). Depuis l'enfance, ma passion pour
+              l'informatique m'a poussé à maîtriser la conception 3D et les
+              moteurs graphiques. Aujourd'hui, je veux créer des applications
+              web et continuer à évoluer dans ce domaine.
+            </p>
+          )}
+          <div
+            className={`fleche ${texteVisible ? "active" : ""}`}
+            onClick={toggleTexte}
+          >
             <span className="text">
               {texteVisible ? "Masquer le texte" : "En savoir plus"}
             </span>
-            <span className={`fleche ${texteVisible ? "active" : ""}`}>
+            <span className="chevrons">
               <BsChevronDoubleDown />
             </span>
           </div>
-          {texteVisible && (
-            <p>
-              Je m'appelle Lyes, je suis un développeur front-end junior. J'ai
-              suivi une formation de développeur web chez Openclassroom qui m'a
-              permis de comprendre réellement le métier de développeur web. Au
-              cours de mon cursus de formation, j'ai aiguisé mes compétences de
-              manière approfondie, notamment grâce aux projets réalisés au cours
-              de la formation. D'autre part, j'ai aussi travaillé mes
-              compétences en parallèle de mon parcours afin d'optimiser mon
-              apprentissage, ce qui fait qu'aujourd'hui je suis capable de créer
-              des applications complètes en React, d'intégrer des maquettes, de
-              créer des sites web en JavaScript vanilla, et j'ai aussi développé
-              des compétences côté back-end dans une moindre mesure (création
-              d'un serveur, d'une API, connexions entre le front et le back,
-              utilisation de frameworks tels que Express.js, utilisation et mise
-              en place d'une base de données comme MongoDB). Je suis passionné
-              par l'informatique depuis l'enfance, mes premiers "travaux"
-              étaient des mods pour des jeux vidéo (je concevais des maps
-              multijoueur et solo par pure passion) ; grâce à cette passion j'ai
-              appris à maîtriser des logiciels de conception 3D tels que 3ds
-              Max, à utiliser des moteurs graphiques, à faire du scriptage, etc.
-              Maintenant, je veux créer des applications web, mettre à profit
-              mes compétences ainsi qu'évoluer de manière constante dans ce
-              domaine qui me passionne tout autant.
-            </p>
-          )}
 
           <button className="cv" title="Télécharger" onClick={downloadPdf}>
             Curriculum vitae <FaDownload />
